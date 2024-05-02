a10da8d16622d1e0443891a79d41833e9543e56c
Hoang Viet Anh <vietanh092004@gmail.com>
2024-05-02 21:11:18 +0700

    Department Controller

--- src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentCotroller.java
+++ src/main/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentCotroller.java
@@ -1,28 +1,37 @@
 package com.dailycodebuffer.Springboottutorial.controller;
 
+import com.dailycodebuffer.Springboottutorial.Eror.DepartmentNotFoundException;
 import com.dailycodebuffer.Springboottutorial.entity.Department;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentService;
 import com.dailycodebuffer.Springboottutorial.service.DepartmentServiceImpl;
+import jakarta.validation.Valid;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 public class DepartmentCotroller {
     @Autowired
     private DepartmentService departmentService;
+
+//    private final Logger logger = LoggerFactory.getLogger(DepartmentCotroller.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+//        logger.info("Inside saveDepartment");
         return departmentService.saveDepartment(department);
     }
-    @GetMapping("departments")
+    @GetMapping("/departments")
     public List<Department> fetchDepartmentList(){
+//        logger.info("Inside saveDepartment list");
         return departmentService.fetchDepartmentsList();
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentByID(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentByID(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
         return departmentService.fetchDepartmentId(departmentId);
     }
 
