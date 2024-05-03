82730d67b1ea22449719eb3ee48ab3153614b0bc
HoangVietAnh09 <vietanh092004@gmail.com>
2024-05-03 21:47:37 +0700

    dependencies

---
+++ src/test/java/com/dailycodebuffer/Springboottutorial/controller/DepartmentCotrollerTest.java
@@ -0,0 +1,20 @@
+package com.dailycodebuffer.Springboottutorial.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DepartmentCotrollerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveDepartment() {
+    }
+
+    @Test
+    void fetchDepartmentByID() {
+    }
+}
