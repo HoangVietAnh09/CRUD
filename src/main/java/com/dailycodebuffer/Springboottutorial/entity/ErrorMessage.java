276c2e7d10fa984a4c5c055923de116937537f24
HoangVietAnh09 <vietanh092004@gmail.com>
2024-05-03 21:55:51 +0700

    return status & message

--- src/main/java/com/dailycodebuffer/Springboottutorial/entity/ErrorMessage.java
+++ src/main/java/com/dailycodebuffer/Springboottutorial/entity/ErrorMessage.java
@@ -1,4 +1,15 @@
 package com.dailycodebuffer.Springboottutorial.entity;
 
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorMessage {
+    private HttpStatus status;
+    private  String message;
+
 }
