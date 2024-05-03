445215eba99d73c896d6ef0045dbd89c68dc0dc8
HoangVietAnh09 <vietanh092004@gmail.com>
2024-05-03 22:02:06 +0700

    Test service

--- src/test/java/com/dailycodebuffer/Springboottutorial/service/DepartmentServiceTest.java
+++ src/test/java/com/dailycodebuffer/Springboottutorial/service/DepartmentServiceTest.java
@@ -1,11 +1,44 @@
 package com.dailycodebuffer.Springboottutorial.service;
 
+import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.repository.DepartmentRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class DepartmentServiceTest {
+    @Autowired
+    private DepartmentService departmentService;
+
+    @MockBean
+    private DepartmentRepository departmentRepository;
+
 
     @BeforeEach
     void setUp() {
+        Department department =
+                                Department.builder().departmentName("IT")
+                                        .departmentAddress("Ha Noi")
+                                        .departmentCode("A1121")
+                                        .departmentID(1L)
+                                        .build();
+        Mockito.when(departmentRepository.findByDepartmentNameIgnoreCase("IT")).thenReturn(department);
     }
+
+    @Test
+    @DisplayName("Get DB on Valia Department Name")
+    public void WhenValidDepartmentNameThenDepartmentShouldFound(){
+        String departmentName = "IT";
+        Department found = departmentService.fetchDepartmentByDepartmentName(departmentName);
+        assertEquals(departmentName, found.getDepartmentName());
+    }
+
 }
